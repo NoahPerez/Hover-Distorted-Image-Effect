5659724ef868c748209a94a4264d6b14994ba2c1
Noah <noahperezlachaud@gmail.com>
2020-02-09 13:06:02 +0000

    "first commit"

---
+++ js/main.js
@@ -0,0 +1,10 @@
+new hoverEffect({
+    parent: document.querySelector('.wrapper'),
+    intensity1: 1,
+    intensity2: 2,
+    speedIn: 1,
+    speedOut: 1,
+    image1: '../img/img_one.jpg',
+    image2: '../img/img_two.jpg',
+    displacementImage: '../img/1.jpg'
+});
